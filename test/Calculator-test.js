8803bce3f9e8469ab1341af6ec3baabdd9d186ae
johnfr14 <jonathan.alyra@gmail.com>
2021-05-20 21:12:55 +0200

    yosh

---
+++ test/Calculator-test.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Calculator", function() {
+let calculator;
+
+  this.beforeEach(async () => {
+    const Calculator = await ethers.getContractFactory("Calculator");
+    calculator = await Calculator.deploy();
+  })
+
+
+  it("should return the addition of both parameters", async function() {
+    expect(await calculator.add(2, 5)).to.equal(7); // Pass
+  });
+
+  it("should return the substraction of both parameters" , async function() {
+    expect(await calculator.sub(5, 2)).to.equal(3); // Pass
+
+  })
+
+  it("should return the multiplication of both parameters" , async function() {
+    expect(await calculator.mul(2, 5)).to.equal(10);// Pass
+    
+  })
+
+  it("should return the division of both parameters" , async function() {
+    expect(await calculator.div(5, 5)).to.equal(5); // Fail should be 1
+    
+  })
+
+  it("should return the modulo of both parameters" , async function() {
+    expect(await calculator.mod(10, 6)).to.equal(4);// Pass
+  })
+});
